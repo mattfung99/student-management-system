819dbab16b163eb55c31228f8ff3d16b592acaa8
Matt Fung <official.matthiasfung@gmail.com>
2021-10-15 17:33:23 -0700

    Upload basic CRUD app

--- bin/integrationTest/spring/resources.groovy
+++
@@ -1,7 +0,0 @@
-import com.app.UserPasswordEncoderListener
-import com.app.UserCredentialsService
-// Place your Spring DSL code here
-beans = {
-    userPasswordEncoderListener(UserPasswordEncoderListener)
-    userDetailsService(UserCredentialsService)
-}
